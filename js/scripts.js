ccb318b09df061e9dbab1e3b04dd8050b9508995
Maggie Harrington <maggie@>
2017-01-18 13:50:55 -0800

    fix visibility issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-  $("#workResponses").hide();
-  $("#activityResponses").hide();
-  $("form#transportationSurvey").submit(function(event){
+$(document).ready(function() {
+  // $("#workResponses").hide();
+  // $("#activityResponses").hide();
+  $("form#transportationSurvey").submit(function(event) {
     event.preventDefault();
 
-    $("input:checkbox[name=workTransportation]:checked").each(function(){
+    $("input:checkbox[name=workTransportation]:checked").each(function() {
       var workTransportationMode = $(this).val();
       $("#workResponses").append(workTransportationMode + "<br>");
     });
 
-    $("input:checkbox[name=activityTransportation]:checked").each(function(){
+    $("input:checkbox[name=activityTransportation]:checked").each(function() {
       var activityTransportationMode = $(this).val();
       $("#activityResponses").append(activityTransportationMode + "<br>");
     });
